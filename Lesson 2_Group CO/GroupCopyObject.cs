14f34e9e78836319ca71889a7e4f2af663acea81
Ben22 <Ben22@DESKTOP-BEEJDLG>
2022-04-01 00:25:11 +0300

    Добавьте файлы проекта.

---
+++ Lesson 2_Group CO/GroupCopyObject.cs
@@ -0,0 +1,35 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_2_Group_CO
+{
+    [TransactionAttribute(TransactionMode.Manual)]
+    public class GroupCopy : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            Reference reference = uidoc.Selection.PickObject(ObjectType.Element, "Выберите группу элементов");
+            Element elem = doc.GetElement(reference);
+            Group group = elem as Group;
+
+            XYZ point = uidoc.Selection.PickPoint("Выберите точку вставки");
+
+            Transaction transaction = new Transaction(doc);
+            transaction.Start("Копирование группы объектов");
+            doc.Create.PlaceGroup(point, group.GroupType);
+            transaction.Commit();
+
+            return Result.Succeeded;
+        }
+    }
+}
